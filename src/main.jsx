e45e8de854b01be845c5624d2a1af44f6fe76cfa
DevDan <ogunniyioreyemi@gmail.com>
2023-11-03 07:57:09 +0100

    changed the position of the cursor

--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,20 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
+import { useEffect } from "react";
+
+useEffect(() => {
+  const cursor = document.querySelector('.custom-cursor');
+
+  document.addEventListener('mousemove', (e) => {
+    cursor.style.left = `${e.pageX}px`;
+    cursor.style.top = `${e.pageY}px`;
+  });
+}, []);
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
+    <div className="custom-cursor"></div>
     <App />
   </React.StrictMode>,
 )
