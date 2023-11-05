8951a77852aa7fb378bcecf6a78181270e09e458
DevDan <ogunniyioreyemi@gmail.com>
2023-11-05 01:09:54 +0100

    change to mouse ripple effect

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { useEffect } from "react";
+import { useState, useEffect, useRef } from "react";
 import Footer from "./components/Footer";
 import Navbar from "./components/Navbar";
 import Features from "./components/Features";
@@ -10,18 +10,65 @@ import Sponsor from "./components/Sponsor";
 import Hero from "./components/Hero";
 
 function App() {  
- useEffect(() => {
-    const cursor = document.querySelector('.custom-cursor');
+  const [ripples, setRipples] = useState([]);
+  const rippleCounter = useRef(0);
 
-    document.addEventListener('mousemove', (e) => {
-      cursor.style.left = `${e.pageX}px`;
-      cursor.style.top = `${e.pageY}px`;
-    });
+  useEffect(() => {
+    const addRipple = (e) => {
+      const rippleContainer = document.querySelector('.ripple-container');
+      if (rippleContainer) {
+        const rect = rippleContainer.getBoundingClientRect();
+        const x = e.clientX - rect.left;
+        const y = e.clientY - rect.top;
+
+        const newRipple = {
+          x,
+          y,
+          id: rippleCounter.current,
+        };
+
+        setRipples((prevRipples) => [...prevRipples, newRipple]);
+        rippleCounter.current += 1;
+
+        setTimeout(() => {
+          setRipples((prevRipples) => prevRipples.filter((r) => r.id !== newRipple.id));
+        }, 1000);
+      }
+    };
+
+    document.addEventListener('mousemove', addRipple);
+
+    return () => {
+      document.removeEventListener('mousemove', addRipple);
+    };
   }, []);
+
 
   return (
     <div className="px-0 md:px-4">
-      <div className="custom-cursor"></div>
+      <div className="relative-container">
+      {/* Your React App content */}
+      {/* Container for the ripple effect */}
+      <div className="ripple-container">
+      <span
+            style={{
+              top: '50%',
+              left: '50%',
+            }}
+            className="ripple"
+          ></span>
+        {ripples.map((ripple) => (
+          <span
+            key={ripple.id}
+            style={{
+              top: ripple.y,
+              left: ripple.x,
+            }}
+            className="ripple"
+          ></span>
+        ))}
+      </div>
+    </div>
       <Navbar />
       <Hero />
       <Features />
